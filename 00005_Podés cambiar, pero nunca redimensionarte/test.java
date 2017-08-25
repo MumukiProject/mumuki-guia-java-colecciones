0e727086d140e0bbc7b01655b1e5a567fcc53e22
franco@mumuki.org <franco@mumuki.org>
2017-08-25 13:11:48 -0400

    Mumuki Export on 2017-08-25 13:11:48 -0400

--- 00005_Podés cambiar, pero nunca redimensionarte/test.java
+++ 00005_Podés cambiar, pero nunca redimensionarte/test.java
@@ -4,24 +4,24 @@ Piso mirador = new Piso();
 
 @Test
 public void se_puede_crear_una_torre_pasando_los_pisos_por_parametro() {
-  new Torre(Arrays.asList(recepcion, comedor, mirador);
+  new Torre(Arrays.asList(recepcion, comedor, mirador));
 }
 
 @Test
 public void si_la_torre_tiene_3_pisos_su_altura_es_3() {
-  Torre torre = new Torre(Arrays.asList(recepcion, comedor, mirador);
+  Torre torre = new Torre(Arrays.asList(recepcion, comedor, mirador));
   Assert.asserEquals(3, torre.altura());
 }
 
 
 @Test
 public void si_la_torre_tiene_2_pisos_su_altura_es_2() {
-  Torre torre = new Torre(Arrays.asList(recepcion, comedor);
+  Torre torre = new Torre(Arrays.asList(recepcion, comedor));
   Assert.asserEquals(2, torre.altura());
 }
 
 @Test
 public void el_primer_piso_es_el_1() {
-  Torre torre = new Torre(Arrays.asList(recepcion, comedor);
+  Torre torre = new Torre(Arrays.asList(recepcion, comedor));
   Assert.asserEquals(1, torre.numeroDePiso(recepcion));
 }
