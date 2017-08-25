8589cc819169628a91f663307a220139c63f50ed
franco@mumuki.org <franco@mumuki.org>
2017-08-25 13:52:33 -0400

    Mumuki Export on 2017-08-25 13:52:33 -0400

--- 00011_Contate otro/description.md
+++ 00011_Contate otro/description.md
@@ -1,1 +1,16 @@
-TODO
+La famosa comediante Sandra se está preparando para una nueva función, y está repasando qué chistes usará. Ya armó una lista tentativa...
+
+```java
+List<Chiste> chistesParaLaFuncion = ....
+```
+
+...y además de cada chiste sabe si `esBueno`. Lo que ahora necesita saber es **cuántos** chistes buenos hay. ¿Podemos ayudarla? ¡Por supuesto!   
+
+> Probá en la consola las siguientes consultas: 
+> 
+> * `ム chistes`
+> * `ム chistes.size()`
+> * `ム chistes.stream().filter(chiste -> chiste.esBueno()).count()`
+> * `ム chistes.stream().filter(chiste -> !chiste.esBueno()).count()`
+> 
+> ¿Qué está haciendo cada una?
---
+++ 00011_Contate otro/extra.java
@@ -0,0 +1,29 @@
+class Chiste {
+  String texto;
+  boolean esBueno;
+  
+  Chiste(String texto, boolean esBueno) {
+    this.texto = texto;
+    this.esBueno = esBueno;
+  }
+  
+  int cantidadDePalabras() {
+    return texto.size();
+  }
+  
+  String getTexto() {
+    return texto;
+  }
+  
+  boolean esBueno() {
+    return esBueno;
+  }
+}
+
+List<Chiste> chistes = Arrays.asList(
+  new Chiste("¿Sabés que le dice un jaguar a otro? ¿'Jaguar you?'", false),
+  new Chiste("Dos presos en la cárcel y uno le dice al otro: - ¿Y vos por qué estás acá? - Por lo mismo que vos, ¡porque no me dejan salir!", false),
+  new Chiste("Éste es un chiste tan malo, que le pega a los chistes mas chicos", true));
+ 
+
+
