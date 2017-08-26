175f358d53226839fdf5fbc017137148bbf0b9d7
franco@mumuki.org <franco@mumuki.org>
2017-08-26 15:45:50 -0400

    Mumuki Export on 2017-08-26 15:45:50 -0400

--- 00013_A cada chancho le llega su foreach/test.java
+++ 00013_A cada chancho le llega su foreach/test.java
@@ -1,1 +1,24 @@
-TODO
+@Test
+public void inicialmente_todas_las_luces_deben_estar_apagadas() {
+  List<Luz> luces = Arrays.asList(new Luz(), new Luz(), new Luz());
+  
+  ArbolDeNavidad arbol = new ArbolDeNavidad(luces);
+  
+  Assert.assertTrue(
+    "todas las luces deberían estar encendidas después de encender al árbol", 
+    luces.stream().allMatch(l -> !l.encendida));
+  
+} 
+
+@Test
+public void encender_prende_todas_las_luces() {
+  List<Luz> luces = Arrays.asList(new Luz(), new Luz(), new Luz());
+  
+  ArbolDeNavidad arbol = new ArbolDeNavidad(luces);
+  arbol.encender();
+  
+  Assert.assertTrue(
+    "todas las luces deberían estar encendidas después de encender al árbol", 
+    luces.stream().allMatch(l -> l.encendida));
+  
+} 
