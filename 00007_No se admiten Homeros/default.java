69dd3d10b2989a455140e737debce0f5d8614a56
fedescarpa+mumuki@gmail.com <fedescarpa+mumuki@gmail.com>
2018-02-21 11:12:47 -0500

    Mumuki Export on 2018-02-21 11:12:47 -0500

---
+++ 00007_No se admiten Homeros/default.java
@@ -0,0 +1,13 @@
+class ClubDeNoHomeros {
+  
+  List<Magio> magio = new List();
+  
+  void agregarMagio(Magio unMagio) {
+    magios.add(unMagio);
+  }
+  
+  int cantidadMagios() {
+    return homeros.size();
+  }
+  
+}
