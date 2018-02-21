8fbe5e78a2c67211d085df9871ca711fe16fea2b
fedescarpa+mumuki@gmail.com <fedescarpa+mumuki@gmail.com>
2018-02-21 10:15:18 -0500

    Mumuki Export on 2018-02-21 10:15:17 -0500

--- 00002_¿Es una lista? ¿Es un array? Es ¡ArrayList!/test.java
+++ 00002_¿Es una lista? ¿Es un array? Es ¡ArrayList!/test.java
@@ -19,6 +19,6 @@ public void si_agregamos_dos_titulos_repetidos_el_tamanio_es_1() {
   Titulo unTitulo = new Titulo();
   alejandria.incorporar(unTitulo);
   alejandria.incorporar(unTitulo);
-  Assert.assertEquals(1, alejandria.tamanio());
+  Assert.assertEquals(, alejandria.tamanio());
 }
 
