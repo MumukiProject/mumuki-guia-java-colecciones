f6d46d43f3cd0e404eded7a551d6de200f9311ce
franco@mumuki.org <franco@mumuki.org>
2018-05-02 13:11:54 -0400

    Mumuki Export on 2018-05-02 13:11:54 -0400

--- 00003_Hay muchas listas/test.java
+++ 00003_Hay muchas listas/test.java
@@ -1,4 +1,22 @@
+ReclamoComun faltaUnaBirome = new ReclamoComun();
+ReclamoComun elMonitorAndaMal = new ReclamoComun();
+ReclamoComun incendioEnPlanta = new ReclamoPrioritario();
+ReclamoComun corteGeneralDeServicio = new ReclamoPrioritario();
+CentroDeAtencion centro = new CentroDeAtencion();
+
 @Test 
 public void compila() {
   
+}
+
+@Test 
+public void agregarEnOrdenDePrioridad() {
+  centro.aceptarReclamo(faltaUnaBirome);
+  centro.aceptarReclamo(corteGeneralDeServicio);
+  centro.aceptarReclamo(elMonitorAndaMal);
+  centro.aceptarReclamo(incendioEnPlanta);
+  Assert.assertEquals(
+    "no es el orden correcto. Debería ser incendioEnPlanta, corteGeneralDeServicio, faltaUnaBirome, elMontorAndaMal",
+    Arrays.asList(incendioEnPlanta, corteGeneralDeServicio, faltaUnaBirome y elMontorAndaMal), 
+    centro.getReclamos()); 
 }
