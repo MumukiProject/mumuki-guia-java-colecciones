cb1daec84fac3e1ad66c157f16029cad4956ed00
gus.trucco@gmail.com <gus.trucco@gmail.com>
2018-09-17 17:05:32 -0400

    Mumuki Export on 2018-09-17 17:05:32 -0400

--- 00007_No se admiten Homeros/test.java
+++ 00007_No se admiten Homeros/test.java
@@ -3,12 +3,6 @@ ClubDeNoHomeros club;
 @Before
 public void setUp() {
   club = new ClubDeNoHomeros();
-  club.integrantes.add("Homero");
-  club.integrantes.add("Homero");
-  club.integrantes.add("Abe");
-  club.integrantes.add("Abe");
-  club.integrantes.add("Burns");
-  club.integrantes.add("Burns");
 }
 
 @Test
@@ -21,4 +15,16 @@ public void el_club_de_no_homeros_tiene_cuatro_integrantes_si_agrego_alguno_que_
   club.integrantes.add("Abe");
   club.integrantes.add("Abe");
   Assert.assertEquals(4, club.cantidadIntegrantes());
+}
+
+@Test
+public void el_club_de_no_homeros_no_tiene_en_cuenta_los_integrantes_repetidos() {
+  club.integrantes.clear();
+  club.integrantes.add("Homero");
+  club.integrantes.add("Homero");
+  club.integrantes.add("Abe");
+  club.integrantes.add("Abe");
+  club.integrantes.add("Burns");
+  club.integrantes.add("Burns");
+  Assert.assertEquals(3, club.cantidadIntegrantes());
 }
